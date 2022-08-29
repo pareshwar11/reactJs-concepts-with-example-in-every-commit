76d20700e040836900fca07bbad5a19cc7fb332a
Pare9977 <pygpp008@gmail.com>
2022-08-29 19:06:30 +0530

    install bootstrap and react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
 import './App.css';
-import style from './custom.module.css'
-
+import React from 'react'
+import Button from 'react-bootstrap/Button';
 function App() {
   return (
     <div className="App">
-      <h1>Style Types In ReactJS, 3 ways to impliment</h1>
-      <h3 className='primary'>Hey This is 1 way to Style--- External File (App.css)</h3>
-      <h3 style={{color:'red', backgroundColor:'black'}}>Hey This is 2nd way to Style--- Inline Style</h3>
-      <h3 className={style.success}>Hey This is 3rd way to Style--- CSS with module (custom.module.css)</h3>
+      <h1 className='text-center'>We are going to learn about How we can install bootstrap and how we can use that</h1>
+      <h3 className='my-5'>1. run this code in terminal :- <b>npm i bootstrap react-bootstrap</b> ( here 2 pakages are, react-bootstrap is different and bootstrap is different library )</h3>
+      <h3>2. copy the module.css file from https://react-bootstrap.github.io/ </h3>
+      <h3>3. Paste it in the index.js file</h3>
+      <h3>4. For using the react-bootstrap  read the official document</h3>
+      <span>You can also use the bootstrap by copy CDN link paste inside the index.html file </span>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
     </div>
   );
 }
