50321b1fd5882f954d51cf1f0d1c9aaa71efe0a1
Pare9977 <pygpp008@gmail.com>
2022-08-29 13:45:32 +0530

    Style Types in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
-import User from './User';
-
+import style from './custom.module.css'
 
 function App() {
   return (
     <div className="App">
-      <User/>
+      <h1>Style Types In ReactJS, 3 ways to impliment</h1>
+      <h3 className='primary'>Hey This is 1 way to Style--- External File (App.css)</h3>
+      <h3 style={{color:'red', backgroundColor:'black'}}>Hey This is 2nd way to Style--- Inline Style</h3>
+      <h3 className={style.success}>Hey This is 3rd way to Style--- CSS with module (custom.module.css)</h3>
     </div>
   );
 }
